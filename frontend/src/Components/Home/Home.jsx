6823907b4f7de6a16ddbfcd869c9156e2dda9e09
root <michaelpopedeveloper@gmail.com>
2019-05-15 16:46:31 -0700

    Video Background

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import Login from '../Login/Login';
 import { connect } from 'react-redux';
-import logo from '../../logo.svg';
 import '../../styles/app.css';
 import { loginUser } from '../../actions/index';
 
@@ -16,14 +14,24 @@ const mapStateToProps = state => {
   }
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            videoURL: 'https://player.vimeo.com/external/276547742.sd.mp4?s=069d4ce8015fee598ba71ae6bfbab699432606e6&profile_id=164&oauth2_token_id=57447761',
+        }
+    }
     render() {
         return (
             <div className="App">
-                {/* <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1>Home!</h1>
-                </header> */}
-                <Login />
+            <video id="background-video" loop autoPlay>
+                <source src={this.state.videoURL} type="video/mp4" />
+                <source src={this.state.videoURL} type="video/ogg" />
+                Your browser does not support the video tag.
+            </video>
+               <h1 className="display-1 pt-5 mt-5 mb-0 pb-0">Pet Pair</h1>
+               <p className="text-secondary mt-0 pt-0">Pairing people, with their favorite animals.</p>
+
+               <button className="btn btn-primary mt-5"> <i class="fas fa-map-marker-alt pr-2"></i> Find Animals In Your Area</button>
             </div>
         );
     }
